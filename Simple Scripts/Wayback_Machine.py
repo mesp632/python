c2f0417c6f41ad9fc9d87fc0e2629e7566dfe8d6
mesp632 <152715449+mesp632@users.noreply.github.com>
2023-12-12 11:22:28 -0700

    Wayback Machine std lib modules
    Script to use Wayback machine to look up a website from a previous date. Uses standard library modules.

---
+++ Simple Scripts/Wayback_Machine.py
@@ -0,0 +1,20 @@
+import webbrowser
+import json
+from urllib.request import urlopen
+
+print("Let's use the Wayback Machine.")
+site = input("Type a website URL: ")
+datetime = input("Type a year, month, and a day formatted like YYYYMMDD: ")
+url = "http://archive.org./wayback/available?url=%s&timestamp=%s" % (site,datetime)
+url_response = urlopen(url)
+url_contents = url_response.read()
+url_text = url_contents.decode("utf-8")
+url_data = json.loads(url_text)
+
+try:
+    old_url = url_data["archived_snapshots"]["closest"]["url"]
+    print("Found this: ", old_url)
+    print("Opening Browser to view: ")
+    webbrowser.open(old_url)
+except:
+    print("Unable to find: ", old_url)
