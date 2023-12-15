84052c68a5ef12f3e80ac964a048ea6afbed3a72
mesp632 <152715449+mesp632@users.noreply.github.com>
2023-12-15 14:32:22 -0700

    python math syntax examples
    contains examples to less used or unique Python math syntax

---
+++ Simple Scripts/python_math_syntax.py
@@ -0,0 +1,16 @@
+#uses type() to find variable type/class
+testint=7 ** 5 #calculated 7exp(5) a.k.a 7^5 in some languages
+print(testint)
+print("7exp(5) = ",type(testint))
+truncatingdivint = 17//5 #truncating division
+print("truncated division of 17/5: ",truncatingdivint)
+print("get variable type/class: ", type(truncatingdivint))
+remainder = 17%5
+print("get remainder of truncated division 17/5: ", remainder)
+print("get the truncated and remainder at the same time(as a tuple): ", divmod(17,5))
+
+#example that a-= 3 is progromatically a=a-3
+a=10
+print("a should be 10: a = ",a)
+a -= 3
+print("a should now be 7: a = ", a)
